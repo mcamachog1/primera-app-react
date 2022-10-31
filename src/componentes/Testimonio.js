972b881c1a546533519c0637fc9210e7b5d26cf1
Maryvi <mcamachog@hotmail.com>
2022-10-31 05:20:17 +0000

    crear componente Testimonio

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,16 +1,21 @@
 import React from "react";
 
-function Testimonio() {
+export function Testimonio() {
 	return (
 		<div className="contenedor-testimonio">
 			<img
 				className="imagen-testimonio"
-				src=''
-			/>
+				src={require("../imagenes/testimonio-Camila.png")}
+				alt="Foto de Camila"/>
+			<div className="contenedor-texto-testimonio">
+				<p className="nombre-testimonio">Camila Briceno</p>		
+				<p className="cargo-testimonio">Estudiante de 5to anio</p>
+				<p className="texto-testimonio">"Excelente el curso, muy buen aprendizaje para crear paginas web 1000% recomendando, ya que es otra herramienta más para tu futuro como profesional crearlas para tu uso personal, y para otros."</p>
+			</div>
+	
 
 		</div>
-
 	)
-
-
 }
+
+// export default Testimonio;
